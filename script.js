21e01a8abeb5b3175937c3c10bc04ffcbbbcc860
sufian-hamuti <31727780+sufian-hamuti@users.noreply.github.com>
2021-09-26 19:56:52 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var range = document.getElementById("textInput").value;
+    var nyears = document.getElementById("years").value;
+    var result = ((p*range)/100)*nyears;
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById('result').innerHTML="if you deposit " + p + ", </br>" +"at an interest rate of "+range+"%.</br>"+
+                                                 "You will receive an amount of "+result+',</br>'+"in the year of "+ (n+5);
 }
+
+function updateTextInput(val) {
+          document.getElementById('textInput').value=val; 
+        }
         
